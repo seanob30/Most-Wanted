7aa83fd5dcf4b45a39cbdc9d0e33ac23e38827a4
Nick Jaeger <jaegernh@uwec.edu>
2017-01-23 13:55:42 -0600

    reorganized code stubs

---
+++ gui.js
@@ -0,0 +1,30 @@
+function initSearch(people){
+	;
+}
+function initSearchByName(){
+	;
+}
+function initSearchByTraits(){
+	;
+}
+function displayResults(){
+	;
+}
+function isNumeric() {
+	;
+}
+function getAge(){
+	;
+}
+function getHeight(){
+	;
+}
+function getWeight(){
+	;
+}
+function getEye(){
+	;
+}
+function getOccupation(){
+	;
+}
