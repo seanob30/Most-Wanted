f79c163d4ba929588819072806e22129b8ff989a
seanob30 <seanobrien2014@gmail.com>
2017-01-23 18:08:25 -0600

    initial commit

--- gui.js
+++ gui.js
@@ -1,14 +1,32 @@
-function initSearch(people){
-	;
+function initSearch(people) {
+    initSearchByTraits(people)
 }
-function initSearchByName(){
-	;
+function initSearchByName(people){
+    var input = prompt("Please enter a first or last name");
+
+    for (var i = 0; i < people.length; i++) {
+        if (people[i]["firstName"] == input || people[i]["lastName"] == input) {
+            displayResults(people, i);
+        }
+    }
 }
-function initSearchByTraits(){
-	;
+function initSearchByTraits(people){
+	var input = prompt("Please enter gender or height or weight or eye color or occupation");
+	var filteredSearch = people.filter(filterByTraits)
+	for (var i = 0; i < filteredSearch.length; i++){
+		displayResults(filteredSearch, i);
+	}
 }
-function displayResults(){
-	;
+function filterByTraits(item){
+	if(item.gender == input || item.height == input || item.weight == input || item.eyeColor == input || item.occupation == input){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+function displayResults(people, i){
+    alert(people[i].firstName + " " + people[i].lastName);;
 }
 function isNumeric() {
 	;
